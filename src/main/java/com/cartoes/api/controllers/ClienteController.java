ed794e3383caea5b6111b5831b512259049a0b43
Gabriel Santos <gabrogolt@gmail.com>
2020-09-05 13:46:15 -0300

    Endpoints padronizadas

--- src/main/java/com/cartoes/api/controllers/ClienteController.java
+++ src/main/java/com/cartoes/api/controllers/ClienteController.java
@@ -1,7 +1,7 @@
 package com.cartoes.api.controllers;
- 
+
 import java.util.Optional;
- 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,83 +13,106 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
- 
+
 import com.cartoes.api.entities.Cliente;
+import com.cartoes.api.response.Response;
 import com.cartoes.api.services.ClienteService;
 import com.cartoes.api.utils.ConsistenciaException;
- 
+
 @RestController
 @RequestMapping("/api/cliente")
 @CrossOrigin(origins = "*")
 public class ClienteController {
- 
-   	private static final Logger log = LoggerFactory.getLogger(ClienteController.class);
- 
-   	@Autowired
-   	private ClienteService clienteService;
- 
-   	
-   	@GetMapping(value = "/{id}")
-   	public ResponseEntity<Cliente> buscarPorId(@PathVariable("id") int id) {
- 
-         	try {
- 
-                	log.info("Controller: buscando cliente com id: {}", id);
-                	
-                	Optional<Cliente> cliente = clienteService.buscarPorId(id);
- 
-                	return ResponseEntity.ok(cliente.get());
- 
-         	} catch (ConsistenciaException e) {
-                	log.info("Controller: Inconsistência de dados: {}", e.getMessage());
-                	return ResponseEntity.badRequest().body(new Cliente());
-         	} catch (Exception e) {
-                	log.error("Controller: Ocorreu um erro na aplicação: {}", e.getMessage());
-                	return ResponseEntity.status(500).body(new Cliente());
-         	}
- 
-   	}
- 
-   	
-   	@GetMapping(value = "/cpf/{cpf}")
-   	public ResponseEntity<Cliente> buscarPorCpf(@PathVariable("cpf") String cpf) {
- 
-         	try {
- 
-                	log.info("Controller: buscando cliente por CPF: {}", cpf);
- 
-                	Optional<Cliente> cliente = clienteService.buscarPorCpf(cpf);
- 
-                	return ResponseEntity.ok(cliente.get());
- 
-         	} catch (ConsistenciaException e) {
-                	log.info("Controller: Inconsistência de dados: {}", e.getMessage());
-                	return ResponseEntity.badRequest().body(new Cliente());
-         	} catch (Exception e) {
-                	log.error("Controller: Ocorreu um erro na aplicação: {}", e.getMessage());
-                	return ResponseEntity.status(500).body(new Cliente());
-         	}
- 
-   	}
- 
-   	
-   	@PostMapping
-   	public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente) {
- 
-         	try {
- 
-                	log.info("Controller: salvando o cliente: {}", cliente.toString());
- 
-                	return ResponseEntity.ok(this.clienteService.salvar(cliente));
- 
-         	} catch (ConsistenciaException e) {
-                	log.info("Controller: Inconsistência de dados: {}", e.getMessage());
-                	return ResponseEntity.badRequest().body(new Cliente());
-         	} catch (Exception e) {
-                	log.error("Controller: Ocorreu um erro na aplicação: {}", e.getMessage());
-                	return ResponseEntity.status(500).body(new Cliente());
-         	}
- 
-   	}
- 
+
+	private static final Logger log = LoggerFactory.getLogger(ClienteController.class);
+
+	@Autowired
+	private ClienteService clienteService;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Response<Cliente>> buscarPorId(@PathVariable("id") int id) {
+
+		Response<Cliente> response = new Response<Cliente>();
+
+		try {
+
+			log.info("Controller: buscando cliente com id: {}", id);
+
+			Optional<Cliente> cliente = clienteService.buscarPorId(id);
+
+			response.setDados(cliente.get());
+
+			return ResponseEntity.ok(response);
+
+		} catch (ConsistenciaException e) {
+
+			log.info("Controller: Inconsistência de dados: {}", e.getMessage());
+			response.adicionarErro(e.getMensagem());
+			return ResponseEntity.badRequest().body(response);
+
+		} catch (Exception e) {
+			log.error("Controller: Ocorreu um erro na aplicação: {}", e.getMessage());
+			response.adicionarErro("Ocorreu um erro na aplicação: {}", e.getMessage());
+			return ResponseEntity.status(500).body(response);
+
+		}
+
+	}
+
+	@GetMapping(value = "/cpf/{cpf}")
+	public ResponseEntity<Response<Cliente>> buscarPorCpf(@PathVariable("cpf") String cpf) {
+
+		Response<Cliente> response = new Response<Cliente>();
+
+		try {
+
+			log.info("Controller: buscando cliente por CPF: {}", cpf);
+
+			Optional<Cliente> cliente = clienteService.buscarPorCpf(cpf);
+
+			response.setDados(cliente.get());
+			return ResponseEntity.ok(response);
+
+		} catch (ConsistenciaException e) {
+
+			log.info("Controller: Inconsistência de dados: {}", e.getMessage());
+			response.adicionarErro(e.getMensagem());
+			return ResponseEntity.badRequest().body(response);
+
+		} catch (Exception e) {
+
+			log.error("Controller: Ocorreu um erro na aplicação: {}", e.getMessage());
+			response.adicionarErro("Ocorreu um erro na aplicação: {}", e.getMessage());
+			return ResponseEntity.status(500).body(response);
+
+		}
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Response<Cliente>> salvar(@RequestBody Cliente cliente) {
+
+		Response<Cliente> response = new Response<Cliente>();
+
+		try {
+
+			log.info("Controller: salvando o cliente: {}", cliente.toString());
+
+			response.setDados(this.clienteService.salvar(cliente));
+			return ResponseEntity.ok(response);
+
+		} catch (ConsistenciaException e) {
+			log.info("Controller: Inconsistência de dados: {}", e.getMessage());
+			response.adicionarErro(e.getMensagem());
+			return ResponseEntity.badRequest().body(response);
+
+		} catch (Exception e) {
+			log.error("Controller: Ocorreu um erro na aplicação: {}", e.getMessage());
+			response.adicionarErro("Ocorreu um erro na aplicação: {}", e.getMessage());
+			return ResponseEntity.status(500).body(response);
+
+		}
+
+	}
+
 }
